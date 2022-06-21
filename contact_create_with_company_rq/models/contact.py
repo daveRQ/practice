d53370603dff46c47c7180c9db9ae9ac7681ab91
daveRQ <42696677+daveRQ@users.noreply.github.com>
2022-06-21 15:20:55 -0500

    update

--- contact_create_with_company_rq/models/contact.py
+++ contact_create_with_company_rq/models/contact.py
@@ -6,15 +6,6 @@ import json
 class ResPartnerCreateWithCompany(models.Model):
     _inherit = 'res.partner'
 
-    company_ids_filter = fields.Many2many('res.company', string='')
-    get_company_ids = fields.Char(compute='_compute_get_company_ids_filter', string='Get COmpany Ids')
-
-    @api.depends('company_ids_filter')
-    def _compute_get_company_ids_filter(self):
-        for rec in self:
-            rec.get_company_ids = True
-            rec.company_ids_filter = self.env.user.company_ids.ids
-
     @api.model
     def default_get(self, fields):
         res = super(ResPartnerCreateWithCompany, self).default_get(fields)
