6f0dee4cf352fe6279b9f31b04bd683ed555da74
daveRQ <42696677+daveRQ@users.noreply.github.com>
2022-06-21 10:11:35 -0500

    its okey

--- contact_create_with_company_rq/models/contact.py
+++ contact_create_with_company_rq/models/contact.py
@@ -6,16 +6,13 @@ import json
 class ResPartnerCreateWithCompany(models.Model):
     _inherit = 'res.partner'
 
-    @api.model_create_multi
-    def create(self, vals):
-        res = super(ResPartnerCreateWithCompany, self).create(vals)
-        return res
-
     @api.model
     def default_get(self, fields):
         res = super(ResPartnerCreateWithCompany, self).default_get(fields)
-        # res['company_id'] = self.env.company.id
-        raise ValidationError(self.env.context.get('company_id'))
+        res['company_id'] = self.env.company.id
         return res
 
-
+    @api.onchange('company_id')
+    def _onchange_company_id(self):
+        if self.env.user not in self.env['res.groups'].search([ ('name', '=', 'Ver Compañia en Contacto') ]).users:
+            raise ValidationError('No puede cambiar la Compañia')
