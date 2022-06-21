570b1b3936959ac8c540eed896f3519b5dcce26f
daveRQ <42696677+daveRQ@users.noreply.github.com>
2022-06-21 09:45:45 -0500

    company_contact

---
+++ contact_create_with_company_rq/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Contact Create With Company Rq',
+    'version': '1.0',
+    'description': 'Product On Hand Change Rq It',
+    'author': 'RQ',
+    'license': 'LGPL-3',
+    'category': 'base',
+    'auto_install': False,
+    'depends': [
+        # '',
+    ],
+    'data': [
+        'views/company.xml'
+        'views/contact.xml'
+    ],
+    'installable': True
+}
---
+++ contact_create_with_company_rq/models/contact.py
@@ -0,0 +1,21 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+import json
+
+
+class ResPartnerCreateWithCompany(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model_create_multi
+    def create(self, vals):
+        res = super(ResPartnerCreateWithCompany, self).create(vals)
+        return res
+
+    @api.model
+    def default_get(self, fields):
+        res = super(ResPartnerCreateWithCompany, self).default_get(fields)
+        # res['company_id'] = self.env.company.id
+        raise ValidationError(self.env.company.id)
+        return res
+
+
